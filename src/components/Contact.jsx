a2d3f0455208cee29b273f8fa9950085aa05fc10
Tony Yuan <jhyn1687@uw.edu>
2022-12-23 01:13:09 -0800

    changed some styling, added dynamic quotes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,46 @@
+import { useEffect, useState } from "react";
 import {
   MdEmail as Email
 } from "react-icons/md";
 
+import { supabase } from "../SupabaseClient";
+
 function Contact() {
+  const [quote, setQuote] = useState("");
+  useEffect(() => {
+    async function getQuote() {
+      try {
+        let { data, error, status } = await supabase
+          .from("random_quote")
+          .select("quote")
+          .limit(1)
+          .single();
+
+        console.log(data);
+
+        if (error && status !== 406) {
+          throw error;
+        }
+
+        if (data) {
+          setQuote(data.quote);
+        }
+      } catch (error) {
+        alert(error.message);
+      }
+    }
+    getQuote()
+  }, []);
+
   return (
     <div id="Contact" className="section flex-row">
       <div className="flex-col">
-        <h1 className="section-title">Let's chat.</h1>
-        <p>I have some wonderful ideas that I haven't even thought of yet.</p>
-        <a href="mailto:jhyuan01@gmail.com">
+        <h1 className="section-title">Let's chat!</h1>
+        <p>{quote}</p>
+        <a href="mailto:jhyuan01@gmail.com" style={{ cursor: "default"}}>
           <div className="flex-col bounce">
           <Email
-            className="icon-link"
-            style={{ fontSize: "2rem", marginTop: "2rem" }}
+            className="email-icon"
           />
           <button className="email-button">
             jhyuan01@gmail.com
