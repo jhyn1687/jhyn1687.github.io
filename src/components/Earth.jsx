ddbd3a0e654b4cce6a4f063440ba614d230a9f95
Tony Yuan <Tonyyuan982@gmail.com>
2023-10-31 00:30:49 -0700

    update prospects

--- src/components/Earth.jsx
+++ src/components/Earth.jsx
@@ -3,8 +3,9 @@ import Globe from 'react-globe.gl'
 
 function Earth() {
   const globeEl = useRef();
-  const objs = [{lat: 22.728689, lng: 113.824020}, {lat: 47.608013, lng: -122.335167}]
-  const arcs = [{startLat: 22.728689, startLng: 113.824020, endLat: 47.608013, endLng: -122.335167}]
+  const objs = [{lat: 22.728689, lng: 113.824020}, {lat: 47.608013, lng: -122.335167}, {lat: 37.554169, lng: -122.313057}]
+  const arcs = [{startLat: 22.728689, startLng: 113.824020, endLat: 47.608013, endLng: -122.335167},
+    {startLat: 47.608013, startLng: -122.335167, endLat: 37.554169, endLng: -122.313057}]
   
   useEffect(() => {
     // Auto-rotate
@@ -19,11 +20,11 @@ function Earth() {
       <Globe
         ref={globeEl}
         arcsData={arcs}
-        arcAltitude={.2}
         objectsData={objs}
         objectAltitude={0}
         height={window.innerHeight}
         width={window.innerWidth * ((window.innerWidth > 992) ? 0.4 : 1)}
+        arcAltitudeAutoScale={0.25}
         backgroundColor="rgba(0,0,0,0)"
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
       />
