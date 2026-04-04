29fb248baa00f471cf2779978ff603eced40bc14
pnat1687 <jhyuan01@gmail.com>
2026-04-04 00:22:22 -0700

    pnpm wrangler deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,19 @@ import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 export default defineConfig({
-  plugins: [cloudflareDevProxy(), tailwindcss(), reactRouter()],
+  plugins: [
+    cloudflareDevProxy(),
+    tailwindcss(),
+    reactRouter(),
+    cloudflare({
+      viteEnvironment: {
+        name: "ssr",
+      },
+    }),
+  ],
   resolve: {
     tsconfigPaths: true,
   },
